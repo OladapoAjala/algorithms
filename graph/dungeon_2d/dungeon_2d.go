c6ba2fcc93a7db17327270b9f1890f105e3f55ab
Dapo Ajala <dapo@sephia.io>
2023-12-24 23:15:17 +0100

    cleaner code

--- graph/dungeon_2d/dungeon_2d.go
+++ graph/dungeon_2d/dungeon_2d.go
@@ -7,16 +7,11 @@ import (
 )
 
 func dungeon(grid [][]string, sr, sc int) [][]int {
-	R := len(grid)
-	C := len(grid[0])
-	dr := []int{-1, 1, 0, 0}
-	dc := []int{0, 0, 1, -1}
-
 	pi := make(map[int]int)
 	rq := queue.NewQueue[int]()
 	cq := queue.NewQueue[int]()
 
-	startNode := (sr * C) + sc
+	startNode := (sr * len(grid[0])) + sc
 	endNode := startNode
 	pi[startNode] = -1
 	foundEnd := false
@@ -28,35 +23,7 @@ func dungeon(grid [][]string, sr, sc int) [][]int {
 		if err != nil {
 			return nil
 		}
-
-		for i := 0; i < 4; i++ {
-			rr := r + dr[i]
-			cc := c + dc[i]
-			neighbour := (rr * C) + cc
-
-			if _, visited := pi[neighbour]; visited {
-				continue
-			}
-			if rr < 0 || cc < 0 {
-				continue
-			}
-			if rr >= R || cc >= C {
-				continue
-			}
-			if grid[rr][cc] == "#" {
-				continue
-			}
-
-			_ = rq.Enqueue(rr)
-			_ = cq.Enqueue(cc)
-			currentNode := (r * C) + c
-			pi[neighbour] = currentNode
-			if grid[rr][cc] == "e" {
-				endNode = neighbour
-				foundEnd = true
-				break
-			}
-		}
+		endNode, foundEnd = exploreNeighbours(grid, pi, r, c, rq, cq)
 		if foundEnd {
 			break
 		}
@@ -71,4 +38,39 @@ func dungeon(grid [][]string, sr, sc int) [][]int {
 	}
 	fmt.Printf("%d\n", endNode)
 	return nil
+}
+
+func exploreNeighbours(grid [][]string, pi map[int]int, r int, c int, rq, cq *queue.Queue[int]) (int, bool) {
+	R := len(grid)
+	C := len(grid[0])
+	dr := []int{-1, 1, 0, 0}
+	dc := []int{0, 0, 1, -1}
+
+	for i := 0; i < 4; i++ {
+		rr := r + dr[i]
+		cc := c + dc[i]
+		neighbour := (rr * C) + cc
+
+		if _, visited := pi[neighbour]; visited {
+			continue
+		}
+		if rr < 0 || cc < 0 {
+			continue
+		}
+		if rr >= R || cc >= C {
+			continue
+		}
+		if grid[rr][cc] == "#" {
+			continue
+		}
+
+		_ = rq.Enqueue(rr)
+		_ = cq.Enqueue(cc)
+		currentNode := (r * C) + c
+		pi[neighbour] = currentNode
+		if grid[rr][cc] == "e" {
+			return neighbour, true
+		}
+	}
+	return -1, false
 }
